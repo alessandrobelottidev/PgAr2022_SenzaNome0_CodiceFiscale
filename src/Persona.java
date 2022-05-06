5cde84487d233eba3c989bb7da5026f415808490
alessandrobelottidev <alessandro.belotti2004@gmail.com>
2022-05-06 19:37:00 +0200

    Modified Persona.java from record to Class so that I can normalize data...

--- src/Persona.java
+++ src/Persona.java
@@ -1,5 +1,19 @@
 import java.time.LocalDate;
 
-public record Persona(int id, String nome, String cognome, Sesso sesso, Comune comune, LocalDate nascita) {
+public class Persona {
+    private final int id;
+    private final StringBuilder nome;
+    private final StringBuilder cognome;
+    private final Sesso sesso;
+    private final Comune comune;
+    private final LocalDate nascita;
 
+    public Persona(int id, String nome, String cognome, Sesso sesso, Comune comune, LocalDate nascita) {
+        this.id = id;
+        this.nome = NominativoNormalizer.normalizza(new StringBuilder(nome.toUpperCase()));
+        this.cognome = NominativoNormalizer.normalizza(new StringBuilder(cognome.toUpperCase()));
+        this.sesso = sesso;
+        this.comune = comune;
+        this.nascita = nascita;
+    }
 }
